e28250d7ff073590b421a7fbc13747ad48ca9089
Kasule John Trevor <82664582+Kasuletrevor@users.noreply.github.com>
2024-10-04 21:57:55 +0300

    adjust settings.py

--- AuroraTech/settings.py
+++ AuroraTech/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-lze*w*p-23ps$y^g)38$l7=@v$taflkmexuv!std6-=v!%p@q%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -83,6 +83,7 @@ DATABASES = {
     }
 }
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
